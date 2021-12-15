f5ec6801775895d72026acd8b10e13dd90ba4c9e
jousethen <jthen93@gmail.com>
2021-12-15 17:52:52 -0500

    Moved search form inside of the container

--- components/UserContainer.jsx
+++ components/UserContainer.jsx
@@ -4,13 +4,22 @@ const fetcher = (url) => fetch(url).then((res) => res.json())
 
 const UserContainer = (props) => {
   //use swr hook to get data from our api
-  const { data, error } = useSwr(`/api/users?searchString=${props.searchString}`, fetcher)
+  const { data, error } = useSwr(`/api/users`, fetcher)
 
   if (error) return <div>Failed to load users</div>
   if (!data) return <div>Loading...</div>
 
   return (
     <div className="user-container">
+      <form className="search">
+        <input
+          type="search"
+          aria-label="Search"
+          width="50px">
+        </input>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
       {data.map(user => (
         <div key={user.id} className="user-card">
           <UserCard user={user} />
