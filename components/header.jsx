21941edd3dd1d64584d5ee8a08d7c9adc9d0a585
jousethen <jthen93@gmail.com>
2021-12-15 14:32:23 -0500

    Created Header Component

---
+++ components/header.jsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import logo from '../public/logo.png'
+
+const Header = () => {
+  const router = useRouter()
+
+
+  return (
+    <div className="header">
+
+    </div>
+  )
+}
+
+export default Header
