595204e312ad62ed48bfa67c9ce47abdabc6e74d
jousethen <jthen93@gmail.com>
2021-12-15 17:39:27 -0500

    User Container will receive the searchString as a prop

--- components/UserContainer.jsx
+++ components/UserContainer.jsx
@@ -4,7 +4,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json())
 
 const UserContainer = (props) => {
   //use swr hook to get data from our api
-  const { data, error } = useSwr('/api/users', fetcher)
+  const { data, error } = useSwr(`/api/users?searchString=${props.searchString}`, fetcher)
 
   if (error) return <div>Failed to load users</div>
   if (!data) return <div>Loading...</div>
