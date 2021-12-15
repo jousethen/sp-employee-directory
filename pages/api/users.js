17fd08994b8c26cb265425b695d922d45010afe3
jousethen <jthen93@gmail.com>
2021-12-15 13:57:24 -0500

    Default return value when no search String. Also added ability to filter by department

--- pages/api/users.js
+++ pages/api/users.js
@@ -3,17 +3,27 @@ import prisma from '../../lib/prisma'
 export default async function handle(req, res) {
   const { searchString } = req.query
   console.log(searchString)
-  const users = await prisma.user.findMany({
-    where: {
-      OR: [
-        {
-          title: { contains: searchString },
-        },
-        {
-          location: { contains: searchString },
-        },
-      ],
-    },
-  })
+
+  let users;
+  if (searchString) {
+    users = await prisma.user.findMany({
+      where: {
+        OR: [
+          {
+            title: { contains: searchString },
+          },
+          {
+            location: { contains: searchString },
+          },
+          {
+            department: { name: { contains: searchString } },
+          },
+        ],
+      },
+    })
+  }
+  else {
+    users = await prisma.user.findMany()
+  }
   res.json(users)
 }
