5ab9b7b619472bd32fc7a0ad146096687eb1656a
jousethen <jthen93@gmail.com>
2021-12-15 21:11:10 -0500

    Create form complete

--- pages/users/create.jsx
+++ pages/users/create.jsx
@@ -4,48 +4,68 @@ import Router from 'next/router'
 import Layout from '../../components/Layout'
 
 const Create = (user) => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [title, setTitle] = useState('')
+  const [location, setLocation] = useState('')
+  const [age, setAge] = useState('')
+  const [department, setDepartment] = useState('')
 
-  //Fire off when the button on the Modal is clicked
-  const handleOnChange = async e => {
-    await fetch(`${process.env.HOST}/api/users/`, {
-      method: 'POST',
-    })
-    await Router.push('/users/')
+  const onHandleSubmit = async e => {
+    e.preventDefault()
+    try {
+      const body = {
+        name: name,
+        email: email,
+        title: title,
+        location: location,
+        age: age,
+        departmentId: department
+      }
+      await fetch(`${process.env.HOST}/api/users`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      })
+      await Router.push(`/`)
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
     <Layout>
-      <form>
+      <form onSubmit={onHandleSubmit}>
         <div className="mb-3">
           <div className="mb-3">
             <label for="nameField" className="form-label">Full Name</label>
-            <input type="text" className="form-control" id="fullname" />
+            <input onChange={e => setName(e.target.value)} type="text" className="form-control" id="fullname" />
           </div>
           <div className="mb-3">
             <label for="titleField" className="form-label">Title</label>
-            <input type="text" className="form-control" id="title" />
+            <input onChange={e => setTitle(e.target.value)} type="text" className="form-control" id="title" />
           </div>
           <div className="mb-3">
             <label for="locationField" className="form-label">Location</label>
-            <input type="text" className="form-control" id="location" />
+            <input onChange={e => setLocation(e.target.value)} type="text" className="form-control" id="location" />
           </div>
           <div className="mb-3">
             <label for="ageField" className="form-label">Age</label>
-            <input type="number" className="form-control" id="age" />
+            <input onChange={e => setAge(e.target.value)} type="number" className="form-control" id="age" />
           </div>
           <label for="emailField" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+          <input onChange={e => setEmail(e.target.value)} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
           <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
         </div>
 
         <div class="form-check">
-          <input className="form-check-input" type="radio" name="flexRadioDefault" id="radio1" />
+          <input onChange={e => setDepartment(4)} className="form-check-input" type="radio" name="flexRadioDefault" id="radio1" />
           <label className="form-check-label" for="flexRadioDefault1">
             Pizza Rogue
           </label>
         </div>
         <div className="form-check">
-          <input className="form-check-input" type="radio" name="flexRadioDefault" id="radio2" checked />
+          <input onChange={e => setDepartment(5)} className="form-check-input" type="radio" name="flexRadioDefault" id="radio2" checked />
           <label className="form-check-label" for="flexRadioDefault2">
             IT
           </label>
